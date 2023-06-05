2df3bf8caf80b0fd062386b68b7e7b7bfa533060
TarifHasan <tariquzzaman.tapon@gmail.com>
2023-06-05 10:32:31 +0600

    footer page is created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
-
     </>
   )
 }
--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -5,9 +5,9 @@ import { HiArrowNarrowLeft } from "react-icons/hi";
 const Error = () => {
     return (
         <div>
-            <img className='h-4/5 mx-auto' src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=740&t=st=1685933704~exp=1685934304~hmac=c370c2f8ad4dca6eaffa4e1a7aafb1bff451b95c858d2e81c1b643f61f85315b" alt="" />
+            <img className='h-3/5 mx-auto' src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=740&t=st=1685933704~exp=1685934304~hmac=c370c2f8ad4dca6eaffa4e1a7aafb1bff451b95c858d2e81c1b643f61f85315b" alt="" />
             <div className='text-center'>
-                <Link to="/" className='btn'><HiArrowNarrowLeft className='w-6 h-6'></HiArrowNarrowLeft> Back To Home</Link>
+                <Link to="/" className='btn bg-yellow-400'><HiArrowNarrowLeft className='w-6 h-6'></HiArrowNarrowLeft> Back To Home</Link>
             </div>
         </div>
     );
