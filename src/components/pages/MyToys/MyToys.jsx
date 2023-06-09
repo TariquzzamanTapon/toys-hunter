53002e44eb8d4745c4701064dfb445203c269f84
TarifHasan <tariquzzaman.tapon@gmail.com>
2023-06-09 10:29:30 +0600

    Data delete successfully from the database

--- src/components/pages/MyToys/MyToys.jsx
+++ src/components/pages/MyToys/MyToys.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 import { AiFillDelete, AiFillEdit, AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const [datas, setDatas] = useState([]);
     const { user, loading } = useContext(AuthContext);
 
     useEffect(() => {
+
         fetch('http://localhost:5000/toys')
             .then(res => res.json())
             .then(data => {
@@ -16,6 +18,45 @@ const MyToys = () => {
     }, []);
 
 
+    // DELETE
+    const handleDelete = (id) => {
+        console.log(id);
+        fetch(`http://localhost:5000/toys/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        if (data.deletedCount > 0) {
+                            const remaining = datas.filter(d => d._id !== id);
+                            setDatas(remaining);
+                        }
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                })
+
+            })
+    }
+
+    // EDIT
+    const handleEdit = (id) => {
+
+    }
 
 
 
@@ -48,8 +89,9 @@ const MyToys = () => {
                                     <td>{toy?.available_quantity}</td>
                                     <td>
                                         <div className='flex'>
-                                            <AiFillEdit className='h-6 w-6 hover:text-warning mx-1'></AiFillEdit>
-                                            <AiFillDelete className='h-6 w-6 hover:text-warning mx-1'></AiFillDelete>
+                                            <AiFillEdit onClick={() => handleEdit(toy?._id)} className='h-6 w-6 hover:text-warning mx-1'></AiFillEdit>
+
+                                            <AiFillDelete onClick={() => handleDelete(toy?._id)} className='h-6 w-6 hover:text-warning mx-1'></AiFillDelete>
                                         </div>
                                     </td>
                                 </tr>
